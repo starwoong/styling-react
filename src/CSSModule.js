5f18919f23906ee1ba567126894734ca75ccd5b4
성웅 <woong@seong-ung-ui-MacBookPro.local>
2020-10-29 17:08:36 +0900

    CSS연습

---
+++ src/CSSModule.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./CSSModule.module.scss";
+
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      Hi, Im <span className="something">CSS Module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
